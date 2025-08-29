a194da465c53b8777e1810c8d7d4c936666fb21e
Aryan Sutar <sutararyan1304@gmail.com>
2025-08-29 20:50:12 +0530

    Created Spring project using initializer with dependencies

---
+++ src/main/java/com/to_do_list_vaadin_springboot/project/ToDoListProjectApplication.java
@@ -0,0 +1,13 @@
+package com.to_do_list_vaadin_springboot.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListProjectApplication.class, args);
+	}
+
+}
